bf86779bfb2c4fcbc4a808760c64ee2780eb4f80
Vlad Borisov <amaranth64@yandex.ru>
2023-03-20 21:31:10 +0400

    android.permission.BLUETOOTH_CONNECT for 33 API. Голову свернешь!

--- app/src/main/java/ru/worklight64/bluetoothterminal/MainActivity.kt
+++ app/src/main/java/ru/worklight64/bluetoothterminal/MainActivity.kt
@@ -1,11 +1,153 @@
 package ru.worklight64.bluetoothterminal
 
-import androidx.appcompat.app.AppCompatActivity
+import android.Manifest
+import android.annotation.SuppressLint
+import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothDevice
+import android.bluetooth.BluetoothManager
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import ru.worklight64.bluetoothterminal.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var form: ActivityMainBinding
+    private var btAdapter: BluetoothAdapter? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        form = ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(form.root)
+        initializeBluetoothOrRequestPermission()
     }
+
+    @SuppressLint("ServiceCast")
+    private fun bluetoothInit(){
+        val btManager = getSystemService(Context.BLUETOOTH_SERVICE) as BluetoothManager
+        btAdapter = btManager.adapter
+
+        initializeBluetoothOrRequestPermission()
+
+    }
+
+    fun bluetoothON(): Boolean {
+        if (btAdapter?.isEnabled == false) {
+            val turnOn = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
+            if (ActivityCompat.checkSelfPermission(
+                    this,
+                    Manifest.permission.BLUETOOTH_CONNECT
+                ) != PackageManager.PERMISSION_GRANTED
+            ) {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S)
+                {
+                    ActivityCompat.requestPermissions(this@MainActivity, arrayOf(Manifest.permission.BLUETOOTH_CONNECT), 2)
+
+                }
+            }
+            startActivity(turnOn)
+        } else {
+            return true
+        }
+        return false
+    }
+    private fun getPairedDevices(){
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.BLUETOOTH_CONNECT) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S)
+            {
+                ActivityCompat.requestPermissions(this@MainActivity, arrayOf(Manifest.permission.BLUETOOTH_CONNECT), 2)
+                return
+            }
+
+        }
+        val pairedDevices: Set<BluetoothDevice>? = btAdapter?.bondedDevices
+        pairedDevices?.forEach{
+            form.tvEmpty.append(it.name)
+        }
+
+    }
+
+
+    private fun initializeBluetoothOrRequestPermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S)
+        {
+            val requiredPermissions = listOf(Manifest.permission.BLUETOOTH_CONNECT, Manifest.permission.BLUETOOTH_SCAN)
+            val missingPermissions = requiredPermissions.filter { permission ->
+                checkSelfPermission(permission) != PackageManager.PERMISSION_GRANTED
+            }
+
+            if (missingPermissions.isEmpty()) {
+                initializeBluetooth()
+            } else {
+                requestPermissions(missingPermissions.toTypedArray(), BLUETOOTH_PERMISSION_REQUEST_CODE)
+            }
+
+//            if (missingPermissions.isEmpty()) {
+//                initializeBluetooth()
+//            } else {
+//                requestPermissions(missingPermissions.toTypedArray(), BLUETOOTH_PERMISSION_REQUEST_CODE)
+//            }
+            //ActivityCompat.requestPermissions(this@MainActivity, arrayOf(Manifest.permission.BLUETOOTH_CONNECT), 2)
+        } else {
+            initializeBluetooth()
+        }
+
+    }
+
+    private fun initializeBluetooth() {
+
+        val btManager = getSystemService(Context.BLUETOOTH_SERVICE) as BluetoothManager
+        btAdapter = btManager.adapter
+
+        if (btAdapter?.isEnabled == false) {
+            val turnOn = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
+//            if (ActivityCompat.checkSelfPermission(
+//                    this,
+//                    Manifest.permission.BLUETOOTH_CONNECT
+//                ) != PackageManager.PERMISSION_GRANTED
+//            ) {
+//                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S)
+//                {
+//                    ActivityCompat.requestPermissions(this@MainActivity, arrayOf(Manifest.permission.BLUETOOTH_CONNECT), 2)
+//                }
+//            }
+            startActivity(turnOn)
+        }
+
+        val pairedDevices: Set<BluetoothDevice>? = btAdapter?.bondedDevices
+        pairedDevices?.forEach{
+            form.tvEmpty.append(it.name)
+        }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        when (requestCode) {
+            BLUETOOTH_PERMISSION_REQUEST_CODE -> {
+                if (grantResults.none { it != PackageManager.PERMISSION_GRANTED }) {
+                    // all permissions are granted
+                    initializeBluetooth()
+                } else {
+                    finish()
+                }
+            }
+            else -> super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        }
+    }
+
+    companion object {
+        private const val BLUETOOTH_PERMISSION_REQUEST_CODE = 9999
+    }
+
 }
